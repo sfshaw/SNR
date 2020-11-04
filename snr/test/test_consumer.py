b34724a421804e001a59dc7a0b9308582d61c3ba
Spencer Shaw <sfshaw@calpoly.edu>
2020-11-03 21:41:23 -0800

    Fix Consumer test class name

--- snr/test/test_consumer.py
+++ snr/test/test_consumer.py
@@ -1,10 +1,11 @@
 import unittest
 from time import sleep
+from typing import Any
 
 from snr.utils.consumer import Consumer
 
 
-class TestStringMethods(unittest.TestCase):
+class TestConsumer(unittest.TestCase):
 
     def test_increment(self):
         self.num: int = 0
@@ -22,37 +23,42 @@ class TestStringMethods(unittest.TestCase):
 
     def test_consumer_start_join(self):
 
-        def action(none) -> None:
+        # TODO: Remove hacky sleeps
+
+        def action(none: Any) -> None:
             pass
 
         consumer = Consumer("test", action, 0.001)
         # self.assertFalse(consumer.is_alive())
         # sleep(0.05)
         # self.assertFalse(consumer.is_alive())
-        
+
         # consumer.start()
         self.assertTrue(consumer.is_alive())
         sleep(0.05)
         self.assertTrue(consumer.is_alive())
-        
+
         consumer.join()
         self.assertFalse(consumer.is_alive())
         sleep(0.05)
         self.assertFalse(consumer.is_alive())
 
     def test_consumer_put(self):
+
+        # TODO: Remove hacky sleeps
+
         self.num: int = 0
 
         def increment(n: int) -> None:
             self.num += n
 
         consumer = Consumer("test", increment, 0.001)
-        
+
         # Stay dead
         # self.assertFalse(consumer.is_alive())
         # sleep(0.05)
         # self.assertFalse(consumer.is_alive())
-        
+
         # Stay alive
         # consumer.start()
         self.assertTrue(consumer.is_alive())
