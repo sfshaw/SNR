a8ead922a8f91ae6febb4feb6cbc06d6190c6279
Spencer Shaw <sfshaw@calpoly.edu>
2020-11-03 18:22:53 -0800

    Refactor config to use interfaces

--- snr/config.py
+++ snr/config.py
@@ -1,22 +1,30 @@
-from typing import Callable, Dict, List
+from typing import Callable, Dict, List, Optional
 
 from snr.factory import Factory
 
 MODE_DEBUG: str = "debug"
 MODE_DEPLOYED: str = "deployed"
 
+ComponentsByRole = Dict[str, List[Factory]]
+ComponentsGetter = Callable[[str], ComponentsByRole]
+
+
+# def get_empty(role: str) -> ComponentsByRole:
+#     empty: ComponentsByRole = {}
+#     return empty
+
 
 class Config:
     def __init__(self,
-                 components_by_role: Dict[str, List[Factory]] = None,
-                 get_components: Callable[[str], List[Factory]] = None
+                 factories: ComponentsByRole = {},
+                 get_factories: Optional[ComponentsGetter] = None
                  ):
-        self.components = components_by_role
-        self.get_components = get_components
-        if (self.components is None) and (self.get_components is None):
+        self.factories = factories
+        self.get_factories = get_factories
+        if (not self.factories) and (not self.get_factories):
             raise Exception("No componets provided")
 
-    def get(self, mode: str) -> Dict[str, List[Factory]]:
-        if self.get_components:
-            return self.get_components(mode)
-        return self.components
+    def get(self, mode: str) -> ComponentsByRole:
+        if self.get_factories:
+            self.factories = self.get_factories(mode)
+        return self.factories
