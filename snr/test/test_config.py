6a8ac4293362b282d7f44a0ed9505c95b2d62c97
Spencer Shaw <sfshaw@calpoly.edu>
2020-11-03 21:39:36 -0800

    Add tests for Config

--- snr/test/test_config.py
+++ snr/test/test_config.py
@@ -1,13 +1,36 @@
 import unittest
 
-from snr.config import Config
+from snr.config import ComponentsByRole, Config
+from snr.endpoint import EndpointFactory
+from snr.node import Node
 
 
 class TestStringMethods(unittest.TestCase):
+    class TestFac(EndpointFactory):
+        def __init__(self):
+            pass
 
-    def test_no_components(self):
+        def get(self, parent_node: Node):
+            return [None]
+
+    def test_empty(self):
         with self.assertRaises(Exception):
-            config = Config()
+            Config()
+
+    def test_one_fac(self):
+        components: ComponentsByRole = {"test_role": [self.TestFac()]}
+        config = Config(factories=components)
+        self.assertEqual(config.get("test_mode"), components)
+
+    def test_getter(self):
+        self.called: bool = False
+
+        def getter(s: str) -> ComponentsByRole:
+            self.called = True
+            return {}
+
+        self.assertEqual(Config(get_factories=getter).get("test_mode"), {})
+        self.assertTrue(self.called)
 
 
 if __name__ == '__main__':
