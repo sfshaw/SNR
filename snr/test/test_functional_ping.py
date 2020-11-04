29ca0fe0ef66703f73b7c7e5d03aec737867f5af
Spencer Shaw <sfshaw@calpoly.edu>
2020-11-03 17:07:01 -0800

    Move functional test into unit tests

---
+++ snr/test/test_functional_ping.py
@@ -0,0 +1,78 @@
+from snr.test.expector import Expector
+import unittest
+from time import time
+from typing import Dict, List
+
+from snr.config import Config
+from snr.endpoint import Endpoint, EndpointFactory
+from snr.node import TASK_TYPE_TERMINATE, Node
+from snr.runner.test_runner import SynchronusTestRunner
+from snr.task import SomeTasks, Task, TaskPriority
+
+
+class PingTestEndpoint(Endpoint):
+    def __init__(self,
+                 parent_node: Node,
+                 name: str,
+                 expector: Expector):
+        super().__init__(
+            parent_node,
+            name,
+            task_producers=[self.produce_task],
+            task_handlers={
+                "ping_test": self.handle_ping_test,
+                "process_ping_test": self.handle_recv_ping
+            })
+        self.parent_node = parent_node
+        self.expector = expector
+        self.produced_task: bool = False
+
+    def produce_task(self) -> SomeTasks:
+        self.expector.call("produce_task")
+        if not self.produced_task:
+            self.produced_task = True
+            return Task("ping_test", priority=TaskPriority.normal)
+        return None
+
+    def handle_ping_test(self, t: Task) -> SomeTasks:
+        self.expector.call("handle_ping_test")
+        self.parent_node.store_data("ping_test", time())
+        return None
+
+    def handle_recv_ping(self, t: Task) -> SomeTasks:
+        self.expector.call("handle_recv_ping")
+        start = self.parent_node.get_data("ping_test")
+        latency: float = time() - float(start)
+        self.info("DDS ping latency: {}", [latency])
+        return Task(TASK_TYPE_TERMINATE, TaskPriority.high, ["all"])
+
+
+class PingTestFactory(EndpointFactory):
+    def __init__(self, expector: Expector):
+        self.expector = expector
+
+    def get(self, parent_node: Node) -> List[Endpoint]:
+        return [PingTestEndpoint(parent_node,
+                                 "ping_test_endpoint",
+                                 self.expector)]
+
+    def __repr__(self) -> str:
+        return "Ping test factory"
+
+
+class TestInternalDDSPing(unittest.TestCase):
+
+    def test_internal_dds_ping(self):
+        expector = Expector({"produce_task": 2,
+                             "handle_ping_test": 1,
+                             "handle_recv_ping": 1, })
+        runner = SynchronusTestRunner(Config(
+            factories={
+                "test": [PingTestFactory(expector)]
+            }))
+        runner.run()
+        expector.assert_satisfied(self)
+
+
+if __name__ == '__main__':
+    unittest.main()
