aada28697eafb7ca5b70d29eeecc6352c93d01db
Spencer Shaw <sfshaw@calpoly.edu>
2020-11-10 11:49:03 -0800

    Add prompt to Debugger

--- snr/utils/debug/debugger.py
+++ snr/utils/debug/debugger.py
@@ -1,3 +1,5 @@
+from enum import Enum
+from sys import stdin
 from typing import Any, List, Union
 
 from snr.settings import Settings
@@ -10,17 +12,47 @@ SLEEP_TIME_S = 0.025
 # 33 ms => 30 Hz cap
 
 
+class PromptState(Enum):
+    NotRequested = 0
+    Requested = 1
+    Completed = 2
+
+
 class Debugger(Consumer):
     """Print debugging framework allowing any thread or process to send lines
     to be printed on stdout. Channels can be used to filter which messages are
     printed. Messages can be formatted.
     """
 
-    def __init__(self, settings: Settings):
+    def __init__(self, settings: Settings) -> None:
         super().__init__("debugger",
-                         print,
+                         self.prompt_or_print,
                          SLEEP_TIME_S)
         self.settings = settings
+        self.prompt_state: PromptState = PromptState.NotRequested
+        self.prompt_message: str = ""
+
+    def prompt_or_print(self, data: str) -> None:
+        if self.prompt_state is PromptState.Requested:
+            print(self.prompt_message)
+            self.prompt_message = stdin.readline()
+            self.prompt_state = PromptState.Completed
+        print(data)
+
+    def prompt(self, prompt_message: str) -> str:
+        '''Called from outside the debugger thread
+        '''
+        if self.prompt_state is PromptState.Requested:
+            print("Waiting for previous propmt to complete")
+            self.catch_up("Prompt")
+        self.prompt_message = prompt_message
+        self.prompt_state = PromptState.Requested
+        print("Prompt requested")
+        self.catch_up("Waiting for prompt")
+        while self.prompt_state is not PromptState.Completed \
+                and not self.prompt_message:
+            self.catch_up("Still waiting for prompt")
+        return self.prompt_message
 
     def debug(self,
               context_name: str,
