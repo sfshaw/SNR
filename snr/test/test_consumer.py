23cd6fc200533ff9c053ee5b00b92201d24eebc9
Spencer Shaw <sfshaw@calpoly.edu>
2021-01-07 00:48:55 -0800

    Update Consumer tests (still flakey)

--- snr/test/test_consumer.py
+++ snr/test/test_consumer.py
@@ -1,11 +1,12 @@
-import unittest
+
 from time import sleep
-from typing import Any
 
+from snr.test.utils.test_base import *
 from snr.utils.consumer import Consumer
+from snr.utils.utils import no_op
 
 
-class TestConsumer(unittest.TestCase):
+class TestConsumer(SNRTestBase):
 
     def test_increment(self):
         self.num: int = 0
@@ -22,14 +23,14 @@ class TestConsumer(unittest.TestCase):
         self.assertEqual(3, self.num)
 
     def test_consumer_start_join(self):
-
         # TODO: Remove hacky sleeps
-
-        def action(none: Any) -> None:
-            pass
         SLEEP_TIME = 0.0002
         CATCH_UP_TIME = SLEEP_TIME * 2
-        consumer = Consumer("test", action, SLEEP_TIME)
+        consumer = Consumer[int]("test_start_join",
+                                 no_op,
+                                 SLEEP_TIME,
+                                 self.stdout.print)
+
         sleep(CATCH_UP_TIME)
         self.assertTrue(consumer.is_alive())
 
@@ -37,44 +38,42 @@ class TestConsumer(unittest.TestCase):
         sleep(CATCH_UP_TIME)
         self.assertTrue(consumer.is_alive())
 
-        consumer.join()
+        consumer.join_from("test complete")
         self.assertFalse(consumer.is_alive())
         sleep(CATCH_UP_TIME)
         self.assertFalse(consumer.is_alive())
 
     def test_consumer_put(self):
-
-        # TODO: Remove hacky sleeps
-
         SLEEP_TIME = 0.0002
-        CATCH_UP_TIME = SLEEP_TIME * 2
 
         self.num: int = 0
 
         def increment(n: int) -> None:
-            self.num += n
+            self.num = self.num + n
 
-        consumer = Consumer("test", increment, SLEEP_TIME)
-        sleep(CATCH_UP_TIME)
+        consumer = Consumer("test_put",
+                            increment,
+                            SLEEP_TIME,
+                            self.stdout.print)
+        consumer.flush()
         self.assertTrue(consumer.is_alive())
 
         self.assertEqual(0, self.num)
         consumer.put(0)
-        sleep(CATCH_UP_TIME)
-        consumer.catch_up("test")
+        consumer.flush()
         self.assertEqual(0, self.num)
 
         consumer.put(1)
-        sleep(CATCH_UP_TIME)
-        consumer.catch_up("test")
+        consumer.flush()
         self.assertEqual(1, self.num)
 
+        self.assertTrue(consumer.is_alive())
         consumer.put(2)
-        sleep(CATCH_UP_TIME)
-        consumer.catch_up("test")
+        consumer.flush()
         self.assertEqual(3, self.num)
+        self.assertTrue(consumer.is_alive())
 
-        consumer.join()
+        consumer.join_from("test complete")
         self.assertFalse(consumer.is_alive())
         self.assertEqual(3, self.num)
 
