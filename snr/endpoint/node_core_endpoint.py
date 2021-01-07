a011d35a09a511cf267c283285815c5343c5c045
Spencer Shaw <sfshaw@calpoly.edu>
2021-01-07 00:49:33 -0800

    Update NodeCoreEndpoint

--- snr/endpoint/node_core_endpoint.py
+++ snr/endpoint/node_core_endpoint.py
@@ -1,7 +1,9 @@
-from typing import Any
+from typing import Any, Dict
 
 from snr.endpoint.endpoint import Endpoint
-from snr.task import SomeTasks, Task
+from snr.task import SomeTasks, Task, TaskHandler
+
+NODE_CORE_NAME_SUFFIX = "_core_endpoint"
 
 TASK_TYPE_TERMINATE = "terminate"
 TASK_TYPE_RELOAD_ENDPOINT = "reload"
@@ -13,26 +15,24 @@ class NodeCore(Endpoint):
                  factory: None,
                  parent_node: Any
                  ) -> None:
+        task_handlers: Dict[str, TaskHandler] = {
+            TASK_TYPE_TERMINATE: self.task_handler_terminate,
+            TASK_TYPE_RELOAD_ENDPOINT: self.task_handler_reload,
+            TASK_TYPE_LIST_ENDPOINTS: self.task_handler_list_endpoints,
+        }
         super().__init__(factory,
                          parent_node,
-                         parent_node.name + "core_endpoint",
-                         task_handlers={
-                             TASK_TYPE_TERMINATE:
-                             self.task_handler_terminate,
-                             TASK_TYPE_RELOAD_ENDPOINT:
-                             self.task_handler_reload,
-                             TASK_TYPE_LIST_ENDPOINTS:
-                             self.task_handler_list_endpoints,
-                         })
+                         parent_node.name + NODE_CORE_NAME_SUFFIX,
+                         task_handlers=task_handlers)
 
     def task_producer(self) -> SomeTasks:
         return None
 
     def task_handler_terminate(self, t: Task) -> SomeTasks:
-        if ("all" in t.val_list
-                or self.parent_node.name in t.val_list
-                or "node" in t.val_list):
-            self.parent_node.set_terminate_flag("terminate_task")
+        # if ("all" in t.val_list
+        #         or self.parent_node.name in t.val_list
+        #         or "node" in t.val_list):
+        self.parent_node.set_terminate_flag("terminate_task")
         return None
 
     def task_handler_reload(self, t: Task) -> SomeTasks:
@@ -49,5 +49,5 @@ class NodeCore(Endpoint):
     def task_handler_list_endpoints(self, t: Task):
         self.info("Listing endpoints:")
         for name in self.parent_node.endpoints.keys():
-            self.info("{}", [name])
+            self.info("\t{}", [name])
         self.debugger.flush()
