f6e626b6d4a2bc2492b5d231a5b1fa5d3a205850
Spencer Shaw <sfshaw@calpoly.edu>
2021-01-13 16:39:36 -0800

    Update TestBase

--- snr/test/utils/test_base.py
+++ snr/test/utils/test_base.py
@@ -1,7 +1,12 @@
 import unittest
 from sys import stdout
+from typing import List
 
+from snr.config import Config, Mode
+from snr.context.silent_stdout import SilentStdOut
 from snr.context.stdout import StdOut
+from snr.context.stdout_consumer import StdOutConsumer
+from snr.endpoint.factory import Factory
 
 
 class SNRTestBase(unittest.TestCase):
@@ -9,7 +14,10 @@ class SNRTestBase(unittest.TestCase):
     def setUp(self) -> None:
         print()
         stdout.flush()
-        self.stdout = StdOut(self.id())
+        self.stdout: StdOutConsumer = SilentStdOut(self.id())
 
     def tearDown(self) -> None:
         self.stdout.join_from("test_teardown")
+
+    def get_config(self, factories: List[Factory]) -> Config:
+        return Config(Mode.TEST, {"test": factories}, self.stdout)
