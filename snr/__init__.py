2e59db270947580fa39d9a86e529c729c622fd6d
Spencer Shaw <sfshaw@calpoly.edu>
2021-01-17 23:29:37 -0800

    Re-export classes for snr_test

--- snr/__init__.py
+++ snr/__init__.py
@@ -1,9 +1,11 @@
+from snr_core.config import Config
 from snr_core.endpoint.endpoint import Endpoint
 from snr_core.endpoint.endpoint_factory import EndpointFactory
 from snr_core.runner.test_runner import SynchronusTestRunner
 from snr_core.task import TaskType
+from snr_core.test.utils.expector import Expector
+from snr_core.test.utils.expector_endpoint import ExpectorEndpointFactory
 from snr_core.test.utils.test_base import *
-from snr_core.config import Config
 
 from snr.io.console.console import LocalConsole
 from snr.io.console.factory import (CommandProcessorFactory,
