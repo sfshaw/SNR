9db24819ece32dd198f68abdc18809f86ed3b979
Spencer Shaw <sfshaw@calpoly.edu>
2021-01-26 15:37:46 -0800

    Update Recorder and factory

--- snr_std/io/recorder/recorder_endpoint.py
+++ snr_std/io/recorder/recorder_endpoint.py
@@ -1,16 +1,13 @@
+import logging
 from typing import List
 
-import jsons
-from snr_core.endpoint.synchronous_endpoint import Endpoint
-from snr_core.endpoint.factory import FactoryBase
+from snr_core.endpoint.endpoint import Endpoint
+from snr_core.factory.factory_base import FactoryBase
 from snr_core.node import Node
-from snr_core.task import SomeTasks, Task, TaskHandlerMap, TaskType
-
-FILE_EXTENSION = ".txt"
-DATA_STORAGE_TASK_HEADER = "process_"
+from snr_core.task import SomeTasks, Task, TaskHandlerMap, TaskId, TaskType
 
 
-class Recorder(Endpoint):
+class RecorderEndpoint(Endpoint):
     def __init__(self,
                  factory: FactoryBase,
                  parent: Node,
@@ -18,16 +15,19 @@ class Recorder(Endpoint):
                  data_names: List[str]):
         super().__init__(factory, parent, name,
                          task_handlers=self.map_handlers(data_names))
-        self.file = open(self.name + FILE_EXTENSION, "w")
+        self.file = open(self.name, "w")
+        self.log.setLevel(logging.DEBUG)
 
-    def task_handler(self, t: Task) -> SomeTasks:
-        self.dbg("Recording task: {}", [t])
-        if DATA_STORAGE_TASK_HEADER in t.name:
-            page = self.parent_node.datastore.get_page(
-                t.name[len(DATA_STORAGE_TASK_HEADER):])
-            self.file.write(jsons.dumps(page))
+    def task_handler(self, t: Task, k: TaskId) -> SomeTasks:
+        self.dbg("Recording task: %s", [t])
+        if t.type is TaskType.process_data:
+            page = self.parent.get_page(t.name)
+            if page:
+                self.file.write(page.to_json())
+            else:
+                self.err("Tried to read non-existant page: %s", t.name)
         else:
-            self.file.write(jsons.dumps(t))
+            self.file.write(t.to_json())
         return None
 
     def map_handlers(self,
@@ -35,6 +35,7 @@ class Recorder(Endpoint):
                      ) -> TaskHandlerMap:
         handlers: TaskHandlerMap = {}
         for data_name in data_names:
+            handlers[(TaskType.process_data, data_name)] = self.task_handler
             handlers[(TaskType.event, data_name)] = self.task_handler
         return handlers
 
--- snr_std/io/recorder/recorder_factory.py
+++ snr_std/io/recorder/recorder_factory.py
@@ -1,22 +1,22 @@
 from typing import List
 
-from snr_core.endpoint import EndpointBase
+from snr_core.endpoint.endpoint import Endpoint
 from snr_core.endpoint.reloadable_factory import ReloadableEndpointFactory
-from snr.io.recorder import recorder
 from snr_core.node import Node
+from snr_std.io.recorder import recorder_endpoint
 
 
 class RecorderFactory(ReloadableEndpointFactory):
     def __init__(self, name: str, data_names: List[str]):
-        super().__init__(recorder, "recorder_factory")
+        super().__init__(recorder_endpoint, "recorder_factory")
         self.endpoint_name = name
         self.data_names = data_names
 
-    def get(self, parent: Node) -> EndpointBase:
-        return recorder.Recorder(self,
-                                 parent,
-                                 self.endpoint_name,
-                                 self.data_names)
+    def get(self, parent: Node) -> Endpoint:
+        return recorder_endpoint.RecorderEndpoint(self,
+                                                  parent,
+                                                  self.endpoint_name,
+                                                  self.data_names)
 
     def __repr__(self) -> str:
         return f"Recorder Factory (data_names: {self.data_names})"
