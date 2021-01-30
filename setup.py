a1ec4904140daae69a6544ab4d24a467f2b523de
Spencer Shaw <sfshaw@calpoly.edu>
2021-01-29 16:11:25 -0800

    Drop requirements.txt and improve setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,40 @@
-import setuptools
+import pathlib
 
-with open('README.md') as f:
-    long_description = f.read()
+from setuptools import find_packages, setup
 
-with open("requirements.txt") as f:
-    requirements = [line for line in f]
+here = pathlib.Path(__file__).parent.resolve()
 
-setuptools.setup(
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
     name='SNR',
-    version='0.4.0',
+    version='0.4.2',
     author='Spencer Shaw',
     author_email='calpolyroboticsclub@gmail.com',
-    packages=setuptools.find_packages(include=["snr"]),
+    packages=find_packages(include=['snr']),
     url='http://github.com/sfshaw-calpoly/SNR',
     license='LICENSE.txt',
     description='Soft-realtime robotics framework for education',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    install_requires=requirements,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'dataclasses>=0.8'          # Dataclasses backport
+        'dataclasses_json>=0.5.2'   # Serialization
+        'numpy>=1.20.0'             # Kalman filter, CV
+        # pygame>=2.0.0               # Controller
+        'pyserial>=3.5'             # Serial connection
+        # pysimplegui == 4.29.0       # GUI
+        'typing>=3.7.4'             # Typing backports, Generics?
+        'typing-extensions>=3.7.4'  # Protocol backport
+    ],
+    extras_require={
+        'dev': [
+            'setuptools',           # Build tool
+            'wheel',                # Build tool
+            'mypy',                 # Type checking
+            'flake8'                # Linter
+        ],
+        'test': ['check-manifest'],
+    },
     python_requires='>=3.6'
 )
