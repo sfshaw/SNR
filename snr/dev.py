525ae81650f32e2214ab049c7ddde864c8fde0da
Spencer Shaw <sfshaw@calpoly.edu>
2021-02-01 13:02:42 -0800

    Prevent dev.py script from running when imported

--- snr/dev.py
+++ snr/dev.py
@@ -1,10 +1,16 @@
 from snr import *
 
-CONSOLE_PORT: int = 54321
-runner = SynchronusTestRunner(
-    Config(Mode.TEST,
-           {"test": [
-               CommandReceiverFactory(CONSOLE_PORT),
-               CommandProcessorFactory()
-           ]}))
-runner.run()
+
+def main():
+    CONSOLE_PORT: int = 54321
+    runner = SynchronusTestRunner(
+        Config(Mode.TEST,
+               {"test": [
+                CommandReceiverFactory(CONSOLE_PORT),
+                CommandProcessorFactory()
+                ]}))
+    runner.run()
+
+
+if __name__ == '__main__':
+    main()
