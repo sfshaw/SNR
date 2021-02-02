832d4c9fafd1ff5f0997e441b8abe2635e37afc1
Spencer Shaw <sfshaw@calpoly.edu>
2021-02-01 23:39:30 -0800

    Add stopwatch endpoint

---
+++ snr/snr_core/utils/stopwatch_endpoint.py
@@ -0,0 +1,16 @@
+from snr.snr_core.base import *
+
+
+class StopwatchEndpoint(Endpoint):
+    def __init__(self,
+                 factory: EndpointFactory,
+                 parent: NodeProtocol,
+                 times: List[float]
+                 ) -> None:
+        super().__init__(factory, parent, "stopwatch_endpoint")
+        self.task_handlers = {TaskType.process_data: self.task_handler}
+        self.times = times
+
+    def task_handler(self, task: Task, key: TaskId) -> SomeTasks:
+        self.times.append(self.parent.get_time())
+        return None
