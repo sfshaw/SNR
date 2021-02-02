14923a5881ff2636d5f118a73e4dd82f6d0dc79d
Spencer Shaw <sfshaw@calpoly.edu>
2021-02-01 23:34:33 -0800

    Import Iterable

--- snr/snr_types/base.py
+++ snr/snr_types/base.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass
 from enum import Enum
 from types import ModuleType
-from typing import (IO, Any, Callable, Deque, Dict, Generic, List, Optional,
-                    TextIO, Tuple, TypeVar, Union)
+from typing import (IO, Any, Callable, Deque, Dict, Generic, Iterable, List,
+                    Optional, TextIO, Tuple, TypeVar, Union)
 
 from dataclasses_json import DataClassJsonMixin
 from typing_extensions import Protocol, runtime_checkable
