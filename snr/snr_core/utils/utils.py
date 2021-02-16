4ff9a239686fbeb61641aed80f2910f239796af5
Spencer Shaw <sfshaw@calpoly.edu>
2021-02-15 23:05:00 -0800

    Remove unused method from utils

--- snr/snr_core/utils/utils.py
+++ snr/snr_core/utils/utils.py
@@ -7,10 +7,3 @@ from snr.snr_types.base import *
 
 def no_op(*args: Any) -> None:
     pass
-
-
-def init_dict(keys: List[str], val: Any) -> Dict[str, Any]:
-    d = {}
-    for k in keys:
-        d[k] = val
-    return d
