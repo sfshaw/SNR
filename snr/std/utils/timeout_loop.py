d5309fe1cf06e64079be76ba2b91f5c302866326
Spencer Shaw <sfshaw@calpoly.edu>
2021-02-26 23:29:27 -0800

    Add configurable task to TimeoutLoop

--- snr/std/utils/timeout_loop.py
+++ snr/std/utils/timeout_loop.py
@@ -7,14 +7,16 @@ class TimeoutLoop(ThreadLoop):
     def __init__(self,
                  factory: LoopFactory,
                  parent_node: NodeProtocol,
-                 timeout_s: float
+                 timeout_s: float,
+                 task: Task,
                  ) -> None:
         super().__init__(factory,
                          parent_node,
                          "timeout_loop")
         self.timeout_s = timeout_s
+        self.task = task
 
     def setup(self) -> None:
         if self.timeout_s > 0:
             time.sleep(self.timeout_s)
-        self.parent.schedule(task_terminate("Timeout"))
+        self.parent.schedule(self.task)
--- snr/std/utils/timeout_loop_factory.py
+++ snr/std/utils/timeout_loop_factory.py
@@ -6,11 +6,19 @@ FAST_TEST_TIMEOUT_MS: float = 10.0
 
 
 class TimeoutLoopFactory(LoopFactory):
-    def __init__(self, seconds: float = 0, ms: float = 0):
+    def __init__(self,
+                 seconds: float = 0,
+                 ms: float = 0,
+                 task: Optional[Task] = None,
+                 ) -> None:
         super().__init__(timeout_loop)
         self.timeout_s = seconds + (ms / 1000)
+        if not task:
+            task = task_terminate("Timeout")
+        self.task: Task = task
 
     def get(self, parent: NodeProtocol) -> LoopProtocol:
         return timeout_loop.TimeoutLoop(self,
                                         parent,
-                                        self.timeout_s)
+                                        self.timeout_s,
+                                        self.task)
