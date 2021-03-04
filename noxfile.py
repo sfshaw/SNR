8e6549700b3e4ef7036b4ce842520aa1b1d27bd2
Spencer Shaw <sfshaw@calpoly.edu>
2021-03-04 11:58:13 -0800

    Try to fix some pip slowness in noxfile

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,8 @@ from typing import List
 
 import nox
 
-supported_python_versions: List[str] = [
+DEFAULT_PYTHON_VERSION: str = '3'
+SUPPORTED_PYTHON_VERSIONS: List[str] = [
     '3.6',
     '3.7',
     '3.8',
@@ -12,22 +13,31 @@ supported_python_versions: List[str] = [
 ]
 
 
-@nox.session(python=supported_python_versions,
+@nox.session(python=DEFAULT_PYTHON_VERSION,
              reuse_venv=True)
-def tests(session: nox.Session):
-    session.install('.[test]')
+def test(session: nox.Session):
+    session.install('-r', 'requirements.txt')
+    session.install('-r', 'requirements-test.txt')
     session.run('pytest', external=False)
 
 
-@nox.session(python='3',
+@nox.session(python=SUPPORTED_PYTHON_VERSIONS,
+             reuse_venv=True)
+def test_all(session: nox.Session):
+    session.install('-r', 'requirements.txt')
+    session.install('-r', 'requirements-test.txt')
+    session.run('pytest', external=False)
+
+
+@nox.session(python=DEFAULT_PYTHON_VERSION,
              reuse_venv=True)
 def mypy(session: nox.Session):
-    session.install('.[mypy]')
+    session.install('mypy')
     session.run('mypy', '-p', 'snr')
 
 
-@nox.session(python='3',
+@nox.session(python=DEFAULT_PYTHON_VERSION,
              reuse_venv=True)
 def lint(session: nox.Session):
-    session.install('.[lint]')
+    session.install('flake8')
     session.run('flake8', 'snr', 'tests')
