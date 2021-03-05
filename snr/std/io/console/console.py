f53bb11d6f2ebcaa7c3c06290e5e64dcec00d679
Spencer Shaw <sfshaw@calpoly.edu>
2021-03-05 00:30:57 -0800

    Update RemoteConsole, remove thread

--- snr/std/io/console/console.py
+++ snr/std/io/console/console.py
@@ -8,38 +8,31 @@ from snr.core.context.root_context import RootContext
 from snr.core.utils.sockets.sockets_wrapper import SocketsWrapper
 
 PROMPT = "> "
+COMMAND_DATA_NAME: str = "console_command"
 
 
-class RemoteConsole(threading.Thread):
+class RemoteConsole(RootContext):
     def __init__(self,
                  server_tuple: Tuple[str, int],
                  commands: Optional[List[str]] = None,
-                 retry_wait_s: float = 0.5
+                 retry_wait_s: float = 0.5,
+                 name: str = "remote_console",
                  ) -> None:
-        super().__init__(target=self.thread_fn,
-                         name="console_thread",
-                         daemon=False)
-        self.root_context = RootContext("remote_console", None, Settings())
+        super().__init__(name, None, Settings())
         self.server_tuple = server_tuple
         self.retry_wait_s = retry_wait_s
         self.input_file = sys.stdin
         if commands:
             self.input_file = io.StringIO("\n".join(commands) + "\n")
         self.prompt_text: str = server_tuple[0] + PROMPT
-        # self.commands: Dict[str, Command] = {
-        #     "exit": self.cmd_exit,
-        #     "task": self.cmd_schedule_task,
-        #     "reload": self.cmd_reload,
-        #     "list": self.cmd_list,
-        # }
         self.__terminate_flag = threading.Event()
         self.connection = SocketsWrapper(
             (socket.create_connection(self.server_tuple),
              self.server_tuple),
-            self.root_context)
-        self.start()
+            self)
+        self.connection.open()
 
-    def thread_fn(self) -> None:
+    def run(self) -> None:
         with self.connection as _:
             while not self.is_terminated():
                 input = self.get_input()
@@ -62,7 +55,7 @@ class RemoteConsole(threading.Thread):
         return input
 
     def handle_input(self, input: str):
-        page = Page("console_cmd", input, self.name, 0)
+        page = Page(COMMAND_DATA_NAME, [*input.split(" ")], self.name, 0)
         self.connection.send(page.serialize())
         if input == "exit":
             self.set_terminate_flag()
@@ -73,10 +66,6 @@ class RemoteConsole(threading.Thread):
     def set_terminate_flag(self):
         self.__terminate_flag.set()
 
-    def join(self, timeout: Optional[float] = None):
-        self.set_terminate_flag()
-        super().join(timeout)
-
 
 class LocalConsole(RemoteConsole):
     def __init__(self,
@@ -86,8 +75,5 @@ class LocalConsole(RemoteConsole):
                  ) -> None:
         super().__init__(("localhost", server_port),
                          commands,
-                         retry_wait_s=retry_wait_s)
-
-
-if __name__ == "__main__":
-    LocalConsole(54321)
+                         retry_wait_s=retry_wait_s,
+                         name="local_console")
