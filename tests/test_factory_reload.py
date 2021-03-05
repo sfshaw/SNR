cc6488674e74a4d1337e23240589eed61c01c238
Spencer Shaw <sfshaw@calpoly.edu>
2021-03-05 15:39:02 -0800

    Test swapping of endpoints from factories

---
+++ tests/test_factory_reload.py
@@ -0,0 +1,41 @@
+from snr import *
+from snr.core.utils.expector_protocol import ExpectorProtocol
+
+from .test_reload import mod_a, mod_b
+
+EndpointUnion = Union[mod_a.EndpointUnderTest,
+                      mod_b.EndpointUnderTest]
+
+
+class FactoryUnderTest(EndpointFactory):
+    def __init__(self,
+                 expector: ExpectorProtocol,
+                 ) -> None:
+        super().__init__(mod_a)
+        self.target = mod_a.EndpointUnderTest
+        self.expector = expector
+
+    def get(self, parent: NodeProtocol) -> Union[mod_a.EndpointUnderTest,
+                                                 mod_b.EndpointUnderTest]:
+        return self.target(self, parent, self.expector)
+
+
+class TestFactoryReload(SNRTestCase):
+    def test_factory_swap(self):
+        '''This test does not test importlib
+        '''
+        with OrderedExpector(["mod_a", "mod_b", "mod_a"], self) as expector:
+            node = self.mock_node()
+            fac = FactoryUnderTest(expector)
+            endpoint = fac.get(node)
+            endpoint.call()
+            fac.reload_targets = [mod_b]
+            fac.target = mod_b.EndpointUnderTest  # type: ignore
+            fac.reload()
+            endpoint = fac.get(node)
+            endpoint.call()
+            fac.reload_targets = [mod_a]
+            fac.target = mod_a.EndpointUnderTest  # type: ignore
+            fac.reload()
+            endpoint = fac.get(node)
+            endpoint.call()
---
+++ tests/test_reload/mod_a.py
@@ -0,0 +1,19 @@
+from snr.core.utils.expector_protocol import ExpectorProtocol
+from snr import *
+
+
+class EndpointUnderTest(Endpoint):
+    def __init__(self,
+                 factory: EndpointFactory,
+                 parent: NodeProtocol,
+                 expector: ExpectorProtocol,
+                 ) -> None:
+        super().__init__(factory, parent, "endpoint_under_test_a")
+        self.expector = expector
+        self.task_handlers: TaskHandlerMap = {}
+
+    def call(self) -> None:
+        self.expector.call("mod_a")
+
+    def join(self):
+        pass
