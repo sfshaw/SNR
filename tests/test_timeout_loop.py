473404c27eab1b5d29af96d2a781f5cd24732a3c
Spencer Shaw <sfshaw@calpoly.edu>
2021-03-15 11:15:29 -0700

    Ease timeout loop test timing for Pi

--- tests/test_timeout_loop.py
+++ tests/test_timeout_loop.py
@@ -4,10 +4,10 @@ import pytest
 
 class TestTimeoutLoop(SNRTestCase):
 
-    @pytest.mark.timeout(0.010)
+    @pytest.mark.timeout(0.030)
     def test_timeout_loop_ms(self) -> None:
         self.run_test_node([TimeoutLoopFactory(ms=1)])
 
-    @pytest.mark.timeout(0.010)
+    @pytest.mark.timeout(0.030)
     def test_timeout_loop_s(self) -> None:
         self.run_test_node([TimeoutLoopFactory(seconds=0.001)])
