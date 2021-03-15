715b4f42083f902a17f32a6870221e1a3e9fa907
Spencer Shaw <sfshaw@calpoly.edu>
2021-03-14 23:43:54 -0700

    Improve test_sockets_listener

--- tests/test_sockets_listener.py
+++ tests/test_sockets_listener.py
@@ -8,30 +8,30 @@ from snr import *
 
 class TestSocketsLsitener(SNRTestCase):
 
-    @pytest.mark.timeout(0.500)
+    @pytest.mark.timeout(0.200)
     def test_sockets_listener_recv(self) -> None:
 
         data_key = "my_data"
-        addr = ("localhost", 54941)
-
         page = Page(data_key, "data", "origin", 0.75)
         expectations: Expectations = {
             (TaskType.process_data, data_key): 1
         }
 
-        with MPExpector(expectations, self) as expector:
-
+        with MPExpector(expectations, self) as expector, \
+                self.create_server(("localhost", 0)) as server_socket:
+            port: int = server_socket.getsockname()[1]
             factories = [
-                SocketsListenerFactory(addr[1]),
+                SocketsListenerFactory((server_socket, port)),
                 ExpectorEndpointFactory(expector,
                                         exit_when_satisfied=True),
             ]
             proc = mp.Process(target=self.run_test_node,
                               args=(factories,))
             proc.start()
-            time.sleep(0.050)
-            with socket.create_connection(addr) as client:
-                sock = SocketsWrapper((client, addr), self.get_context())
+            time.sleep(0.010)
+            with socket.create_connection(("localhost", port)) as client:
+                sock = SocketsWrapper((client, None),
+                                      self.get_context())
                 sock.send(page.serialize())
 
             proc.join(0.100)
@@ -41,29 +41,30 @@ class TestSocketsLsitener(SNRTestCase):
                 proc.terminate()
                 raise e
 
-    @pytest.mark.timeout(0.500)
+    @pytest.mark.timeout(0.200)
     def test_sockets_listener_send(self) -> None:
 
         data_key = "my_data"
-        addr = ("localhost", 54849)
         page = Page(data_key, "data", "origin", 0.75)
         expectations: Expectations = {
             (TaskType.process_data, data_key): 1
         }
 
-        with MPExpector(expectations, self) as expector:
-
+        with MPExpector(expectations, self) as expector, \
+                self.create_server(("", 0)) as server_socket:
+            port = server_socket.getsockname()[1]
             factories = [
-                SocketsListenerFactory(addr[1], [data_key]),
+                SocketsListenerFactory((server_socket, port),
+                                       [data_key]),
                 ExpectorEndpointFactory(expector,
                                         exit_when_satisfied=True),
             ]
             proc = mp.Process(target=self.run_test_node,
                               args=(factories,))
             proc.start()
-            time.sleep(0.050)
-            with socket.create_connection(addr) as client:
-                sock = SocketsWrapper((client, addr), self.get_context())
+            time.sleep(0.010)
+            with socket.create_connection(("localhost", port)) as client:
+                sock = SocketsWrapper((client, ("", port)), self.get_context())
                 sock.send(page.serialize())
                 response = sock.recv()
                 assert response
