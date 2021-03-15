f1d67250452a013c8ae6150bef21a89c2b4e9ea2
Spencer Shaw <sfshaw@calpoly.edu>
2021-03-14 23:42:08 -0700

    Improve test_replayer_chronological

--- tests/test_replayer_chronological.py
+++ tests/test_replayer_chronological.py
@@ -7,64 +7,53 @@ from snr import *
 
 class TestReplayerChronological(SNRTestCase):
 
-    @pytest.mark.timeout(0.750)
+    @pytest.mark.timeout(0.200)
     def test_replayer_chronological(self):
 
-        time_step_s: float = 0.025
+        time_step_s: float = 0.015
+        num_data_points = 5
+        data_keys = ["raw_data" + str(i) for i in range(num_data_points)]
+        expectations = [(TaskType.store_page, key) for key in data_keys]
+        expected_pages = [Page(key, key, "test_node", time_step_s * (i + 1))
+                          for i, key in enumerate(data_keys)]
 
-        with self.ordered_expector([
-            (TaskType.store_page, "raw_data1"),
-            (TaskType.store_page, "raw_data2"),
-            (TaskType.store_page, "raw_data3"),
-            (TaskType.store_page, "raw_data4"),
-            (TaskType.store_page, "raw_data5"),
-        ]) as ordered_expector:
+        times: List[float] = []
 
-            with self.temp_file("input.tmp") as input, \
-                    self.temp_file("output.tmp") as output:
+        with self.ordered_expector(expectations) as ordered_expector, \
+                self.temp_file() as input, \
+                self.temp_file() as output:
 
-                expected_pages = [
-                    Page("raw_data1", "data1", "test_node", time_step_s),
-                    Page("raw_data2", "data2", "test_node", time_step_s * 2),
-                    Page("raw_data3", "data3", "test_node", time_step_s * 3),
-                    Page("raw_data4", "data4", "test_node", time_step_s * 4),
-                    Page("raw_data5", "data5", "test_node", time_step_s * 5),
-                ]
+            with input.open() as f:
+                for page in expected_pages:
+                    f.write(page.serialize().decode())
+                    f.write("\n")
+            input.assertExists()
 
-                with input.open() as f:
-                    for page in expected_pages:
-                        f.write(page.serialize().decode())
-                        f.write("\n")
-                input.assertExists()
+            self.run_test_node([
+                ReplayerLoopFactory(input.path),
+                RecorderEndpointFactory(output.path, data_keys),
+                StopwatchEndpointFactory(times),
+                ExpectorEndpointFactory(ordered_expector,
+                                        exit_when_satisfied=True),
+            ])
 
-                times: List[float] = []
+            # Check output file
+            output.assertExists()
+            with open(output.path) as f:
+                actual_pages = [Page.deserialize(f.readline())
+                                for _ in range(len(expected_pages))]
+                logging.getLogger("Page").setLevel(
+                    logging.CRITICAL)  # Supress error
+                self.assertIsNone(Page.deserialize(f.readline()))
+                self.assertEqual(expected_pages, actual_pages)
 
-                self.run_test_node([
-                    ReplayerFactory(input.path,
-                                    exit=True),
-                    RecorderFactory(output.path,
-                                    ["raw_data1",
-                                     "raw_data2",
-                                     "raw_data3",
-                                     "raw_data4",
-                                     "raw_data5"]),
-                    StopwatchEndpointFactory(times),
-                    ExpectorEndpointFactory(ordered_expector),
-                ])
-
-                self.assertEqual(times, sorted(times))
-                time_diffs: List[float] = []
-                for i in range(len(times) - 1):
-                    time_diffs.append(times[i + 1] - times[i])
-                avg = sum(time_diffs)/len(time_diffs)
-                self.assertGreater(avg, time_step_s / 2)
-                self.assertLess(avg, time_step_s * 2)
-
-                output.assertExists()
-                with open(output.path) as f:
-                    actual_pages = [Page.deserialize(f.readline())
-                                    for _ in range(len(expected_pages))]
-                    logging.getLogger("Page").setLevel(
-                        logging.CRITICAL)  # Supress error
-                    self.assertIsNone(Page.deserialize(f.readline()))
-                    self.assertEqual(expected_pages, actual_pages)
+        # Check timing
+        self.assertEqual(times, sorted(times))
+        time_diffs = [times[i + 1] - times[i]
+                      for i in range(len(times) - 1)]
+        avg = sum(time_diffs) / len(time_diffs)
+        msg = f"\nTimes: {times}\nTime diffs: {time_diffs}"
+        print(msg)
+        self.assertAlmostEqual(avg, time_step_s,
+                               delta=time_step_s / num_data_points,
+                               msg=msg)
