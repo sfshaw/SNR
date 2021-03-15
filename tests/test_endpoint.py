1c78d93e0d7d247d7575c4c15f7a1c814d2cdb57
Spencer Shaw <sfshaw@calpoly.edu>
2021-03-15 10:51:00 -0700

    Expand endpoint testing

--- tests/test_endpoint.py
+++ tests/test_endpoint.py
@@ -1,38 +1,115 @@
+from typing import Callable, Optional
+
 from snr import *
 
 
-class EndpointUnderTest(Endpoint):
+class InvalidEndpointNoBegin(Endpoint):
     def __init__(self,
                  factory: EndpointFactory,
                  parent: AbstractNode,
-                 expector: ExpectorProtocol,
+                 ) -> None:
+        super().__init__(factory, parent, "invalid_endpoint_no_begin")
+
+    def halt(self) -> None:
+        pass
+
+    def terminate(self) -> None:
+        pass
+
+
+class InvalidEndpointNoHalt(Endpoint):
+    def __init__(self,
+                 factory: EndpointFactory,
+                 parent: AbstractNode,
+                 ) -> None:
+        super().__init__(factory, parent, "invalid_endpoint_no_halt")
+
+    def begin(self) -> None:
+        pass
+
+    def terminate(self) -> None:
+        pass
+
+
+class InvalidEndpointNoTerminate(Endpoint):
+    def __init__(self,
+                 factory: EndpointFactory,
+                 parent: AbstractNode,
+                 ) -> None:
+        super().__init__(factory, parent, "invalid_endpoint_no_terminate")
+
+    def begin(self) -> None:
+        pass
+
+    def halt(self) -> None:
+        pass
+
+
+class ValidEndpoint(Endpoint):
+    def __init__(self,
+                 factory: EndpointFactory,
+                 parent: AbstractNode,
+                 expector: Optional[ExpectorProtocol],
                  ) -> None:
         super().__init__(factory, parent, "endpoint_under_test")
         self.task_handlers: TaskHandlerMap = {}
         self.expector = expector
 
     def begin(self) -> None:
-        self.expector.call("begin")
+        if self.expector:
+            self.expector.call("begin")
 
     def halt(self) -> None:
-        self.expector.call("halt")
+        if self.expector:
+            self.expector.call("halt")
 
     def terminate(self) -> None:
-        self.expector.call("terminate")
+        if self.expector:
+            self.expector.call("terminate")
 
 
-class EndpointUnderTestFactory(EndpointFactory):
-    def __init__(self, expector: ExpectorProtocol) -> None:
+class ValidEndpointFactory(EndpointFactory):
+    def __init__(self, expector: Optional[ExpectorProtocol] = None) -> None:
         super().__init__()
         self.expector = expector
 
     def get(self, parent: AbstractNode) -> Endpoint:
-        return EndpointUnderTest(self, parent, self.expector)
+        return ValidEndpoint(self, parent, self.expector)
 
 
 class TestEndpoint(SNRTestCase):
 
-    def test_endpoint(self):
+    def test_invalid_construction_fails(self):
+        fac = ValidEndpointFactory()
+        node = self.mock_node()
+
+        def construct(invalid_constructor: Callable[
+            [EndpointFactory,
+             AbstractNode,
+             Optional[ExpectorProtocol]], None],
+        ) -> None:
+            invalid_constructor(fac, node, None)
+
+        self.assertRaises(TypeError,
+                          construct,
+                          InvalidEndpointNoBegin)  # type: ignore
+        self.assertRaises(TypeError,
+                          construct,
+                          InvalidEndpointNoHalt)  # type: ignore
+        self.assertRaises(TypeError,
+                          construct,
+                          InvalidEndpointNoTerminate)  # type: ignore
+
+    def test_valid_construction(self):
+        valid_endpoint = ValidEndpoint(ValidEndpointFactory(),
+                                       self.mock_node(),
+                                       None)
+        self.assertTrue(isinstance(valid_endpoint,
+                                   AbstractComponent))  # type: ignore
+        self.assertTrue(isinstance(valid_endpoint,
+                                   AbstractEndpoint))  # type: ignore
+
+    def test_endpoint_methods(self):
         expectations = {
             "begin": 1,
             "halt": 1,
@@ -41,5 +118,5 @@ class TestEndpoint(SNRTestCase):
         with self.expector(expectations) as expector:
             self.run_test_node([
                 TimeoutLoopFactory(),
-                EndpointUnderTestFactory(expector),
+                ValidEndpointFactory(expector),
             ])
