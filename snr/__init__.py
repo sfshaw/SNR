45a97320af79cce18251b70d5178addeeb373fb3
Spencer Shaw <sfshaw@calpoly.edu>
2021-03-14 22:24:54 -0700

    Export renamed interfaces module

--- snr/__init__.py
+++ snr/__init__.py
@@ -3,7 +3,7 @@ framework, including implementing components and `snr.core.Factories and
 running `snr.core.node.Node`s
 '''
 from snr.core import *
-from snr.protocol import *
+from snr.interfaces import *
 from snr.std_mods import *
 from snr.type_defs import *
 from snr.utils import *
