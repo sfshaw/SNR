45af160a9a7fc57324d5f2ec39e27feb6b37e562
Spencer Shaw <sfshaw@calpoly.edu>
2021-03-14 23:38:21 -0700

    Improve TestContext

--- tests/test_context.py
+++ tests/test_context.py
@@ -1,8 +1,17 @@
+from typing import TypeVar
+
 from snr import *
 
 
 class TestContext(SNRTestCase):
     def test_context_handler_no_profiler(self):
-        context = Context("test_context", Settings(), None, Timer())
-        result = context.profile(self.test_name, lambda arg: arg, "hello")
+
+        T = TypeVar('T')
+
+        def no_op(input: T) -> T:
+            return input
+
+        context: AbstractContext = Context(
+            "test_context", Settings(), None, Timer())
+        result = context.profile(self.test_name, no_op, "hello")
         self.assertEqual("hello", result)
