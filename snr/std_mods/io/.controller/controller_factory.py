68ed22f8f71a89318c3f0949e4174d4d6ffb3b4e
Spencer Shaw <sfshaw@calpoly.edu>
2021-03-14 22:59:23 -0700

    Comment out controller

--- snr/std_mods/io/.controller/controller_factory.py
+++ snr/std_mods/io/.controller/controller_factory.py
@@ -1,13 +1,15 @@
-from snr.core import *
-from snr.std_mods.io.controller import controller
+# from snr.core import *
+# from snr.interfaces import *
+
+# from . import controller
 
 
-class ControllerFactory(LoopFactory):
-    def __init__(self, output_data_name: str):
-        super().__init__("Controller Factory")
-        self.output_data_name = output_data_name
+# class ControllerFactory(LoopFactory):
+#     def __init__(self, output_data_name: str):
+#         super().__init__()
+#         self.output_data_name = output_data_name
 
-    def get(self, parent: NodeProtocol) -> LoopProtocol:
-        return controller.Controller(self,
-                                     parent,
-                                     self.output_data_name)
+#     def get(self, parent: AbstractNode) -> ThreadLoop:
+#         return controller.Controller(self,
+#                                      parent,
+#                                      self.output_data_name)
