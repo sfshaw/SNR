1984c071a4423db1e1c7b350b48945d10302a96b
Spencer Shaw <sfshaw@calpoly.edu>
2021-03-14 22:15:39 -0700

    Comment out camera code

--- snr/std_mods/.camera/camera_config.py
+++ snr/std_mods/.camera/camera_config.py
@@ -1,20 +1,20 @@
 
-from enum import Enum
+# from enum import Enum
 
 
-class CameraConfig:
-    def __init__(self, name: str, server_port: int, camera_num: int):
-        self.name = name
-        self.server_port = server_port
-        self.camera_num = camera_num
+# class CameraConfig:
+#     def __init__(self, name: str, server_port: int, camera_num: int):
+#         self.name = name
+#         self.server_port = server_port
+#         self.camera_num = camera_num
 
 
-class ManagerRole(Enum):
-    Source = 0
-    Receiver = 1
+# class ManagerRole(Enum):
+#     Source = 0
+#     Receiver = 1
 
-    def as_str(self):
-        if (self is ManagerRole.Source):
-            return "source"
-        else:
-            return "receiver"
+#     def as_str(self):
+#         if (self is ManagerRole.Source):
+#             return "source"
+#         else:
+#             return "receiver"
