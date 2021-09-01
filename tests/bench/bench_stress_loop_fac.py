b7d435d7d340de04b27d405718923e11c3010d00
Spencer Shaw <sfshaw@calpoly.edu>
2021-08-31 17:24:38 -0700

    Prevent benchmark overwhelming scheduler

--- tests/bench/bench_stress_loop_fac.py
+++ tests/bench/bench_stress_loop_fac.py
@@ -17,7 +17,9 @@ class StressorLoop(ThreadLoop):
                  name: str,
                  enabled: bool,
                  ) -> None:
-        super().__init__(factory, parent, name)
+        super().__init__(factory, parent, name,
+                         max_tick_rate_hz=10)
+        self.enabled = enabled
 
     def setup(self) -> None:
         pass
@@ -35,7 +37,7 @@ class StressorLoop(ThreadLoop):
 
 class StressorLoopFactory(LoopFactory):
     def __init__(self,
-                 max_loops: int = 1000,
+                 max_loops: int = 10,
                  time_limit_s: float = 0.500,
                  ) -> None:
         super().__init__()
@@ -60,7 +62,7 @@ class StressorLoopFactory(LoopFactory):
 
 class BenchStressLoopFac(SNRTestCase):
 
-    def test_stress_endpoint_init(self):
+    def test_stress_loop_init(self):
         stressor_fac = StressorLoopFactory(max_loops=1,
                                            time_limit_s=0.050)
         times: List[float] = []
@@ -77,32 +79,30 @@ class BenchStressLoopFac(SNRTestCase):
         self.assertEqual(stressor_fac.num_children, 1)
         self.assertEqual(len(times), 1)
 
-    # def test_stress_endpoint(self):
-    #     time_target_s: float = 2.000
-    #     stressor_fac = StressorLoopFactory(max_loops=100,
-    #                                        time_limit_s=time_target_s)
-    #     times: List[float] = []
-    #     timer = Timer()
-    #     node: AbstractNode = Node(
-    #         "test",
-    #         self.get_config([
-    #             stressor_fac,
-    #             TimeoutLoopFactory(seconds=time_target_s),
-    #             StopwatchEndpointFactory(times,
-    #                                      [TaskType.terminate]),
-    #         ], Mode.DEBUG))
-    #     node.log.setLevel(logging.WARNING)
-    #     node.loop()
-    #     t_s = timer.current_s()
-    #     print("\nStressed with:\n",
-    #           f"\t{stressor_fac.num_children} stressor loops\n",
-    #           f"\t{stressor_fac.calls} stressor factory calls\n",
-    #           f"\tTerminate expected at {time_target_s * 1000:.0f} ms\n",
-    #           f"\t{times[0] * 1000:.3f} ms terminate handled\n",
-    #           f"\t{t_s * 1000:.3f} ms total time\n",
-    #           )
-    #     if node.profiler:
-    #         print(node.profiler.dump())
+    def test_stress_loop(self):
+        time_target_s: float = 0.250
+        stressor_fac = StressorLoopFactory(max_loops=10,
+                                           time_limit_s=time_target_s)
+        times: List[float] = []
+        timer = Timer()
+        node: AbstractNode = self.get_test_node([
+            stressor_fac,
+            TimeoutLoopFactory(seconds=time_target_s),
+            StopwatchEndpointFactory(times,
+                                     [TaskType.terminate]),
+        ])
+        node.log.setLevel(logging.WARNING)
+        node.loop()
+        t_s = timer.current_s()
+        print("\nStressed with:\n",
+              f"\t{stressor_fac.num_children} stressor loops\n",
+              f"\t{stressor_fac.calls} stressor factory calls\n",
+              f"\tTerminate expected at {time_target_s * 1000:.0f} ms\n",
+              f"\t{times[0] * 1000:.3f} ms terminate handled\n",
+              f"\t{t_s * 1000:.3f} ms total time\n",
+              )
+        if node.profiler:
+            print(node.profiler.dump())
 
 
 if __name__ == '__main__':
