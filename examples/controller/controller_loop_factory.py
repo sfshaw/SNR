b9c0a667163fa4dd299a8965a436d26f59f47ec5
Spencer Shaw <sfshaw@calpoly.edu>
2021-09-06 17:59:13 -0700

    Avoid importing pygame

--- examples/controller/controller_loop_factory.py
+++ examples/controller/controller_loop_factory.py
@@ -1,19 +1,19 @@
 from snr import *
 
-import controller_loop
-
 
 class ControllerLoopFactory(LoopFactory):
 
     output_data: DataKey
 
     def __init__(self, output_data: DataKey = "raw_controller_input") -> None:
-        super().__init__([controller_loop])
+        super().__init__()
         self.output_data = output_data
 
     def get(self,
             parent: AbstractNode,
-            ) -> controller_loop.ControllerLoop:
+            ) -> ThreadLoop:
+        import controller_loop
+        self.reload_targets = [controller_loop]
         return controller_loop.ControllerLoop(self,
                                               parent,
                                               self.output_data)
